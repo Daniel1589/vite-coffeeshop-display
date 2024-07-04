4e127e9cbcaf8d3a38fa7192d6aa1090d382a4c6
Daniel1589 <daniel1589@edu.acc.ac.il>
2024-07-04 10:16:55 +0300

    Made alotta changes, not worth mentioning.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { NavBar } from "./components/NavBar";
+import { MainPage } from "./components/MainPage";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+      <MainPage />
+    </div>
+  );
 }
 
 export default App;
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,12 +1,10 @@
 import { TopSection } from "./TopSection";
 import { MainSection } from "./MainSection";
-import { ProductList } from "./ProductList";
 export const MainPage = () => {
   return (
     <div>
       <TopSection />
       <MainSection />
-      <ProductList />
     </div>
   );
 };
