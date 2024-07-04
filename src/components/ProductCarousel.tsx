1948313efd7b95b0b092da286268fed46f3040c5
Daniel1589 <daniel1589@edu.acc.ac.il>
2024-07-04 13:36:47 +0300

    Added the dreaded ProductCarousel.tsx, such a headache.

--- src/components/ProductCarousel.tsx
+++ src/components/ProductCarousel.tsx
@@ -5,7 +5,7 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 const ProductCarousel = () => {
   const [startIndex, setStartIndex] = useState(0);
   const products = productsData.products;
-  const itemsPerPage = 4; // Number of items to display per page
+  const itemsPerPage = 4;
 
   const nextProducts = () => {
     const newStartIndex = (startIndex + 1) % products.length;
@@ -18,42 +18,49 @@ const ProductCarousel = () => {
   };
 
   return (
-    <div className="flex items-center justify-center bg-[#fefbf1] relative">
-      <div className="py-4  px-12   bg-[#fefbf1] rounded-lg overflow-hidden">
-        <div className="flex w-[1750px] gap-[20px]">
-          {[...products, ...products, ...products]
-            .slice(startIndex, startIndex + itemsPerPage)
-            .map((product, index) => (
-              <div key={index} className="w-full">
-                <div className="relative ">
-                  <img
-                    src={product.src}
-                    alt={product.name}
-                    className="w-full h-102 object-cover "
-                  />
-                  <div className="">
-                    <div className="text-2xl">{product.name}</div>
+    <div className="py-20 bg-[#f2f1ed]">
+      <div className="flex bg-[#f2f1ed] relative">
+        <div className="py-4  px-12   bg-[#f2f1ed]  ">
+          <div className="flex w-[1750px] gap-[20px]">
+            {[...products, ...products, ...products]
+              .slice(startIndex, startIndex + itemsPerPage)
+              .map((product, index) => (
+                <div key={index} className="w-full">
+                  <div className="relative ">
+                    <img
+                      src={product.src}
+                      alt={product.name}
+                      className="w-full h-102 object-cover"
+                    />
+                    <div className="">
+                      <div className="text-2xl ">{product.name}</div>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-gray-600">{product.price}</p>
                   </div>
                 </div>
-                <div className="p-4">
-                  <p className="text-gray-700 mb-2">{product.price}</p>
-                </div>
-              </div>
-            ))}
-        </div>
-        <div className="p-4 flex justify-between">
-          <IoIosArrowBack
-            className="absolute top-[250px] left-2 text-black size-6 hover:cursor-pointer"
-            values="Previous"
-            onClick={prevProducts}
-          />
+              ))}
+          </div>
+          <div className="p-4 flex justify-between">
+            <IoIosArrowBack
+              className="absolute top-[250px] left-2 text-black size-6 hover:cursor-pointer"
+              values="Previous"
+              onClick={prevProducts}
+            />
 
-          <IoIosArrowForward
-            className="absolute top-[250px] right-2 text-black size-6 hover:cursor-pointer"
-            values="Previous"
-            onClick={nextProducts}
-          />
+            <IoIosArrowForward
+              className="absolute top-[250px] right-2 text-black size-6 hover:cursor-pointer"
+              values="Previous"
+              onClick={nextProducts}
+            />
+          </div>
         </div>
+      </div>
+      <div className="flex justify-center">
+        <button className="border-[1px] border-black text-center text-[35px] font-normal bottom-[75px] right-[100px] py-[25px] px-[70px] bg-[#fff7d8]">
+          SHOP ALL
+        </button>
       </div>
     </div>
   );
