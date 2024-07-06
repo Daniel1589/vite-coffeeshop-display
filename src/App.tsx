e61cd14a7ae804aeba00f16d156e3dddd62d072e
Daniel1589 <daniel1589@edu.acc.ac.il>
2024-07-06 16:39:57 +0300

    Put footer into App.tsx instead of MainPage.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { NavBar } from "./components/NavBar";
 import { MainPage } from "./components/MainPage";
+import { Footer } from "./components/Footer";
 function App() {
   return (
     <div>
       <NavBar />
       <MainPage />
+      <Footer />
     </div>
   );
 }
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,14 +1,12 @@
 import { TopSection } from "./TopSection";
 import { MainSection } from "./MainSection";
 import { ProductCarousel } from "./ProductCarousel";
-import { Footer } from "./Footer";
 export const MainPage = () => {
   return (
     <div>
       <TopSection />
       <MainSection />
       <ProductCarousel />
-      <Footer />
     </div>
   );
 };
